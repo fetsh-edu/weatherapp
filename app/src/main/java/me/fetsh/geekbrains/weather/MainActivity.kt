6e387ee24e8aa1a89b1d41948c8aaad44a561d97
fetsh-edu <fetsh.me@gmail.com>
2021-06-03 15:03:23 +0300

    Track CONNECTIVITY_ACTION

--- app/src/main/java/me/fetsh/geekbrains/weather/MainActivity.kt
+++ app/src/main/java/me/fetsh/geekbrains/weather/MainActivity.kt
@@ -1,10 +1,36 @@
 package me.fetsh.geekbrains.weather
 
+import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 import me.fetsh.geekbrains.weather.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val br: BroadcastReceiver = object : BroadcastReceiver() {
+
+        private var snackbar : Snackbar? = null
+
+        override fun onReceive(context: Context, intent: Intent) {
+            val hasConnection = !intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false)
+            if (hasConnection) {
+                snackbar?.dismiss()
+            } else {
+                val view : View? = (context as Activity).findViewById(R.id.container)
+                view?.let {
+                    snackbar = Snackbar.make(it, context.getString(R.string.no_connection), Snackbar.LENGTH_INDEFINITE)
+                    snackbar?.show()
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +40,14 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerReceiver(br, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+    }
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(br)
     }
 }
